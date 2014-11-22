d730dec54f57b77ef5e4f21c572d91c96bde4c07
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-22 14:27:30 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="assets/styles/styles.scss" />
+  </head>
+  <body>
+    <header></header>
+    <div id="content">
+      <form method="POST">
+        <input type="text" placeholder="Your Name" name="name" />
+        <input type="email" placeholder="Your Email" name="email_address" />
+        <input type="submit" value="Submit" name="submit" />
+      </form>
+    </div>
+  </body>
+</html>
